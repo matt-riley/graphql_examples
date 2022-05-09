96e4dae113bebff9c5b813ba088135d5fe67d6f5
Matt Riley <matt-riley@users.noreply.github.com>
2022-05-09 17:24:30 +0100

    Delete typescript/lastfm directory

--- typescript/lastfm/src/resolvers/root.ts
+++
@@ -1,9 +0,0 @@
-import { GraphQLRequestContext } from 'apollo-server-plugin-base';
-
-export default {
-  Query: {
-    lastfm(_: null, __: null, context: GraphQLRequestContext) {
-      return context;
-    },
-  },
-};
